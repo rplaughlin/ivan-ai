95ef50e28c6bdfc0c8ce2bb0e716c932ec4c6dc6
rplaughlin <rplaughlin986@gmail.com>
2016-12-02 10:46:19 -0500

    Added a heading and a form

--- default.php
+++ default.php
@@ -1,3 +1,18 @@
+<html>
+<head>
+  <title font-family:sans-serif>IVAN AI</title>
+  <hr>
+</head>
+<body>
+  <div>
+    This is the online IVAN AI. To use it, just enter a sentence:
+    <form>
+      <input type="text" name="raw">
+      <input type="submit" name="Submit">
+    </form>
+  </div>
+</body>
+</html>
 <?php
 
 ?>
